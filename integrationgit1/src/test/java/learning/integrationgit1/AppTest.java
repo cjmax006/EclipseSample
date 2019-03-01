d29c91803325f94d015e63bf38533df04a5553c3
Chandan Jha <cjmax.006@gmail.com>
2019-03-02 00:07:05 +0530

    This is the test commit - from Eclipse

---
+++ integrationgit1/src/test/java/learning/integrationgit1/AppTest.java
@@ -0,0 +1,29 @@
+package learning.integrationgit1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+
+    protected WebDriver driver;
+    public void sampleCheck() throws InterruptedException {
+    	
+    	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+    	String eTitle = "Meet Gurr99";
+    	String aTitle = "";
+    	
+    	driver.get("http://www.guru99.com");
+    	driver.manage().window().maximize();
+    	aTitle = driver.getTitle();
+    	
+    	if(aTitle.contentEquals(eTitle))
+    		System.out.println("Test passed");
+    		else
+    			System.out.println("Test Failed");
+    	
+    	
+    	driver.close();   	
+    	
+    }
+}
